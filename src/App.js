b57bc992cae00c99788aa5b2939c8e6ce409e749
unknown <orkatzir@gmail.com>
2023-11-14 11:48:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import AuthLogin from './authLogin';
+import AuthProfile from './authProfile';
+import AuthLogout from './AuthLogout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <> 
+   <AuthLogin></AuthLogin> 
+   <AuthProfile></AuthProfile>
+   <AuthLogout></AuthLogout>
+   </>
+   );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Auth0Provider
+    domain="dev-mv08fv6pdavwwh0d.us.auth0.com"
+    clientId="uF0diF2ocE76jpO3uI6K0xYKJiy88HKq"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
